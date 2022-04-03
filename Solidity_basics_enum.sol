c20f82e64e4a18b77db8856cace213b187255fa8
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-04-03 08:11:39 -0300

    Update Solidity_basics_enum.sol

--- Solidity_basics_enum.sol
+++ Solidity_basics_enum.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
+// This contact illustrates the use for the enumerated list, in solidity we definit with enum
+
 contract CheckDoor {
 
     constructor() {
         door = DoorCondition.Open;       // this construction sets the default state of the door when the contract is created
     }
 
-    // enum restrict how many values a variable can have
+    // enum restricts how many values a variable can have
 
     enum DoorCondition {Closed , Open}   // this variable can only have the value Open or value Closed
     DoorCondition door;                  // declare a variable door to keep track of
